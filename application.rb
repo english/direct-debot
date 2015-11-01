bcd4cefeb1309c1b15f1f5e69fe49ad38cc9dd7d
Jamie English <jamienglish@gmail.com>
2015-11-01 19:16:25 +0000

    Inline building auth client

--- application.rb
+++ application.rb
@@ -1,7 +1,7 @@
 require 'lotus/router'
+require 'oauth2'
 require 'coach'
 require 'prius'
-require_relative 'lib/oauth_client'
 require_relative 'middleware/injector'
 require_relative 'routes/index'
 require_relative 'routes/slack_messages'
@@ -14,14 +14,15 @@ Sequel.extension(:migration)
 module Application
   def self.build(db)
     router       = build_router
-    oauth_client = OAuthClient.build
+    oauth_client = build_oauth_client
+    store        = DB::Store.new(db)
 
     Sequel::Migrator.run(db, 'db/migrations')
 
     Rack::Builder.new do
       use Middleware::Injector, router: router,
                                 oauth_client: oauth_client,
-                                store: DB::Store.new(db)
+                                store: store
       run router
     end
   end
@@ -38,5 +39,17 @@ module Application
       post '/api/slack/messages', to: Coach::Handler.new(Routes::SlackMessages)
       get '/api/gc/callback', to: Coach::Handler.new(Routes::GCCallback), as: :gc_callback
     end
+  end
+
+  private_class_method def self.build_oauth_client
+    client_id         = Prius.get(:gc_client_id)
+    client_secret     = Prius.get(:gc_client_secret)
+    connect_url       = Prius.get(:gc_connect_url)
+    authorize_path    = Prius.get(:gc_connect_authorize_path)
+    access_token_path = Prius.get(:gc_connect_access_token_path)
+
+    OAuth2::Client.new(client_id, client_secret, site: connect_url,
+                                                 authorize_url: authorize_path,
+                                                 token_url: access_token_path)
   end
 end
