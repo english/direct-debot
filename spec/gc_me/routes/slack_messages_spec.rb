820e937e2697609519c17c4e6eca77ddb7b883a3
Jamie English <jamienglish@gmail.com>
2015-11-02 21:49:12 +0000

    Fill out spec

--- spec/gc_me/routes/slack_messages_spec.rb
+++ spec/gc_me/routes/slack_messages_spec.rb
@@ -1,69 +1,93 @@
 require 'active_support/concern'
 require 'action_dispatch/http/request'
-require 'lotus/router'
 require 'json_schema'
+require_relative '../../../lib/gc_me/oauth_client'
 require_relative '../../../lib/gc_me/routes/slack_messages'
-require_relative '../../../lib/gc_me/db/store'
 
 RSpec::Matchers.define :validate do |data|
   match { |schema| schema.validate(data).first }
 end
 
+RSpec.describe GCMe::Routes::SlackMessages::SCHEMA do
+  let(:valid_params) do
+    {
+      'token'        => 'gIkuvaNzQIHg97ATvDxqgjtO',
+      'team_id'      => 'T0001',
+      'team_domain'  => 'example',
+      'channel_id'   => 'C2147483705',
+      'channel_name' => 'test',
+      'user_id'      => 'U2147483697',
+      'user_name'    => 'Steve',
+      'command'      => '/gc-me',
+      'text'         => '£10 from @jane'
+    }
+  end
+
+  let(:schema) { JsonSchema.parse!(GCMe::Routes::SlackMessages::SCHEMA) }
+
+  it 'knows what valid params are' do
+    expect(schema).to validate(valid_params)
+  end
+
+  it "validates 'authorise' messages" do
+    expect(schema).to validate(valid_params.merge('text' => 'authorise'))
+    expect(schema).to_not validate(valid_params.merge('text' => 'authorize'))
+  end
+
+  it "validates 'payment' messages" do
+    valid_amounts      = ['10', '0.1', '1.50']
+    valid_currencies   = ['€', '£']
+    invalid_currencies = ['', 'a', '$']
+
+    valid_currencies.product(valid_amounts).each do |(currency, amount)|
+      data = valid_params.merge('text' => "#{currency}#{amount} from someone")
+
+      expect(schema).to validate(data)
+    end
+
+    invalid_currencies.product(valid_amounts).each do |(currency, amount)|
+      data = valid_params.merge('text' => "#{currency}#{amount} from someone")
+
+      expect(schema).to_not validate(data)
+    end
+  end
+end
+
 RSpec.describe GCMe::Routes::SlackMessages do
-  context 'schema validation' do
-    let(:valid_params) do
-      {
-        'token'        => 'gIkuvaNzQIHg97ATvDxqgjtO',
-        'team_id'      => 'T0001',
-        'team_domain'  => 'example',
-        'channel_id'   => 'C2147483705',
-        'channel_name' => 'test',
-        'user_id'      => 'U2147483697',
-        'user_name'    => 'Steve',
-        'command'      => '/gc-me',
-        'text'         => '£10 from @jane'
-      }
-    end
+  subject(:slack_messages) { GCMe::Routes::SlackMessages.new(context) }
 
-    let(:schema) { JsonSchema.parse!(GCMe::Routes::SlackMessages::SCHEMA) }
+  let(:context) do
+    {
+      request: instance_double(ActionDispatch::Request, params: params),
+      oauth_client: oauth_client
+    }
+  end
 
-    it 'knows what valid params are' do
-      expect(schema).to validate(valid_params)
-    end
+  let(:oauth_client) { instance_double(GCMe::OAuthClient) }
 
-    it "validates 'authorise' messages" do
-      expect(schema).to validate(valid_params.merge('text' => 'authorise'))
-      expect(schema).to_not validate(valid_params.merge('text' => 'authorize'))
-    end
+  describe 'given an authorise message' do
+    let(:params) { { 'user_id' => 'slack-user-id', text: 'authorise' } }
 
-    it "validates 'payment' messages" do
-      valid_amounts      = ['10', '0.1', '1.50']
-      valid_currencies   = ['€', '£']
-      invalid_currencies = ['', 'a', '$']
+    it 'returns a gc oauth link' do
+      expect(oauth_client).
+        to receive(:authorise_url).
+        with('slack-user-id').
+        and_return('https://authorise-url')
 
-      valid_currencies.product(valid_amounts).each do |(currency, amount)|
-        data = valid_params.merge('text' => "#{currency}#{amount} from someone")
+      status, _headers, body = slack_messages.call
 
-        expect(schema).to validate(data)
-      end
-
-      invalid_currencies.product(valid_amounts).each do |(currency, amount)|
-        data = valid_params.merge('text' => "#{currency}#{amount} from someone")
-
-        expect(schema).to_not validate(data)
-      end
+      expect(status).to eq(200)
+      expect(body.first).to eq('<https://authorise-url|Click me!>')
     end
   end
 
-  # describe 'given an authorise message' do
-  #   it 'returns a gc oauth link' do
+  describe 'given a payment message' do
+    let(:params) { { 'user_id' => 'slack-user-id', text: 'authorise' } }
 
-  #   end
-  # end
+    context 'with a @slackuser recipient'
 
-  # describe 'given a payment message' do
-  #   it '' do
+    context 'with an someone@email.com address'
 
-  #   end
-  # end
+    context "with a recipient that doesn't exist"
+  end
 end
