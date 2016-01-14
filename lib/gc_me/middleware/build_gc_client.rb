4c813a7ab9fe4acc8392023c04176492969adce7
Jamie English <jamienglish@gmail.com>
2016-01-14 13:30:30 -0400

    pass a timeout to gocardless pro ruby client

--- lib/gc_me/middleware/build_gc_client.rb
+++ lib/gc_me/middleware/build_gc_client.rb
@@ -13,8 +13,10 @@ module GCMe
       provides :gc_client
 
       def call
+        connection_options = { request: { timeout: 1 } }
         pro_client = GoCardlessPro::Client.new(environment: config.fetch(:gc_environment),
-                                               access_token: gc_access_token)
+                                               access_token: gc_access_token,
+                                               connection_options: connection_options)
 
         provide(gc_client: GCClient.new(pro_client))
 
